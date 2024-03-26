d52d7f9225787a93d71f9adbcfa3900d67c9ad95
Neville Khabuchi <nevillekhabuchi@gmail.com>
2024-03-26 11:10:28 +0300

    New Change

--- myAgrovet.js
+++ myAgrovet.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const mysql=require('mysql');
 const dotenv=require('dotenv');
+const nodemailer=require('nodemailer');
 const session=require('express-session');
 
 dotenv.config({path:'./important.env'});
@@ -8,10 +9,10 @@ dotenv.config({path:'./important.env'});
 const app=express();
 app.set('view engine','ejs');
 app.use(express.static('public'));
-//Parse URL-encoded bodies as sent by HTML 
+//Middleware for parsing URL-encoded form data
 app.use(express.urlencoded({extended:true}));
 
-//Connecting to the database
+//Setting up MySQL connection
 const db=mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -19,7 +20,7 @@ const db=mysql.createConnection({
     database: process.env.DATABASE,
     // port: process.env.PORT
 });
-//Checking for connection errors
+//Connect to MySQL
 db.connect((error)=>{
     if(error){
         console.log('Error connecting to the database',error)
@@ -29,7 +30,18 @@ db.connect((error)=>{
             console.log("Server running on port 3000");
         })
     }
-})
+});
+//Setting up Nodemailer transporter
+const transporter=nodemailer.createTransport({
+    service:'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,//Use 'true' for port 465, 'false' for all other ports
+    auth: {
+        user: 'nevillekhabuchi@gmail.com',//Sender's gmail address
+        pass: 'dgem nunl zzzp egda'//App password from gmail account
+    }
+});
 //Rendering the home page
 app.get('/',(req,res)=>{
     res.render('main',{title:'Landing page'})
@@ -38,6 +50,10 @@ app.get('/',(req,res)=>{
 app.get('/signup',(req,res)=>{
     res.render('signup',{title:'Sign up',errorMessage:null});
 });
+//Rendering the login page
+app.get('/login',(req,res)=>{
+    res.render('login',{title:'Log in Page'});
+})
 //Posting the signup page details
 app.post('/signup',(req,res)=>{
     const{username,phoneNumber,email,password}=req.body;
@@ -61,12 +77,38 @@ app.post('/signup',(req,res)=>{
                     return;
                 }
                 else{
-                    res.send('Sign up successful');
+                    const mailOptions={
+                        from: {
+                            name: 'NEVOLINE AGROVET',
+                            address: 'nevillekhabuchi@gmil.com'
+                        },
+                        to: email,
+                        subject: 'SUCCESSFUL SIGN UP TO NEVOLINE AGROVET',
+                        html: `<p>
+                                Welcome <b>${username}</b> to NEVOLINE AGROVET, where farming meets innovation!<br>
+                                We're here to empower farmers with cutting-edge  solutions for every 
+                                aspect of their farming.<br> From crop management to livestock care, we're 
+                                dedicated to helping you thrive in agriculture.<br> Step in a world of tailored 
+                                solutions and expert guidance.<br><br> Let's cultivate success together.
+                                    </p>`
+                    }
+                    //Send email
+                    transporter.sendMail(mailOptions,(error,info)=>{
+                        if(error){
+                            console.log('Error sending email: ',error);
+                            return;
+                        }
+                        else{
+                            console.log('Email sent: ',info.response);
+                        }
+                    });
+                    res.send('Sign up successfull');
                 }
             })
         }
+        else{
+            const errorMessage="Already registered";
+            return res.render('signup',{title:'Sign up',errorMessage});
+        }
     })
-})
-app.use((req,res) => {
-    res.send("Page error");
 })
