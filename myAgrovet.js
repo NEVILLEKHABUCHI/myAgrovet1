54e0c8bcdb1297fc427fdcdae9f2a888bd3f6998
Neville Khabuchi <nevillekhabuchi@gmail.com>
2024-03-31 12:43:17 +0300

    New Change

--- myAgrovet.js
+++ myAgrovet.js
@@ -18,7 +18,7 @@ const db=mysql.createConnection({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE,
-    port: process.env.PORT
+    // port: process.env.PORT
 });
 //Connect to MySQL
 db.connect((error)=>{
